ba98644ad1139dfaa2d8beaf945cafa8b5592683
jesussmile <pullbackwild@gmail.com>
2016-01-28 21:24:03 +0545

    added recycler view

--- app/src/main/java/com/example/pannam/retrofit2demo/MainActivity.java
+++ app/src/main/java/com/example/pannam/retrofit2demo/MainActivity.java
@@ -1,38 +1,81 @@
 package com.example.pannam.retrofit2demo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.pannam.retrofit2demo.adapter.FlowerAdapter;
+import com.example.pannam.retrofit2demo.controller.RestManager;
+import com.example.pannam.retrofit2demo.model.Flower;
+import com.example.pannam.retrofit2demo.model.adapter.FlowerAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView;
+    private RestManager mManager;
+    private FlowerAdapter mFlowerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+//for RecyclerView
         configViews();
+//getting retrofit from controller / RestManager
+        mManager = new RestManager();
+        Call<List<Flower>> listCall = mManager.getFlowerService().getAllFlowers();
+        //call enqueue in main thread not execute
+        listCall.enqueue(new Callback<List<Flower>>() {
+            @Override
+            public void onResponse(Response<List<Flower>> response) {
+                //add flowers to adapter
+                if (response.isSuccess()) {
+                    List<Flower> flowerList = response.body();
+                    for (int i = 0; i < flowerList.size(); i++) {
+                        Flower flower = flowerList.get(i);
+                        mFlowerAdapter.addFlower(flower);
+                    }
 
 
+                } else {
+                    int sc = response.code();
+                    switch (sc){
+
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
+
+        //to cancel from button
+       // listCall.cancel();
     }
 
     private void configViews() {
-     mRecyclerView =(RecyclerView)this.findViewById(R.id.recyclerView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setRecycledViewPool(new RecyclerView.RecycledViewPool());
+        mFlowerAdapter = new FlowerAdapter();
+
         mRecyclerView.setAdapter(new FlowerAdapter());
     }
-
 
 
     @Override
